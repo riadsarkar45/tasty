77da2c9d61c942acea274ccc273dfa0443f54739
unknown <riadsarkar45@gmail.com>
2025-09-29 17:43:59 +0600

    middileware created for private routes

---
+++ src/hooks/AxiosPrivate.tsx
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const axiosPrivate = axios.create({
+  baseURL: "http://127.0.0.1:10000",
+  // baseURL: "https://tasty-server-2-1.onrender.com",
+  withCredentials: true,
+});
+
+axiosPrivate.interceptors.response.use(
+  response => response,
+  error => {
+    if (error?.response?.status === 401) {
+      console.log("Unauthorized! Logging out...");
+    }
+    return Promise.reject(error);
+  }
+)
+const useAxiosPrivate = () => axiosPrivate;
+
+export default useAxiosPrivate;
