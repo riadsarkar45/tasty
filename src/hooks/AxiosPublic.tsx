220bc29337088720ac2645c3611490ad7265a833
unknown <riadsarkar45@gmail.com>
2025-10-26 16:45:23 +0600

    @base url changed

--- src/hooks/AxiosPublic.tsx
+++ src/hooks/AxiosPublic.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:3000",
-  // baseURL: "https://tasty-server-2-2.onrender.com",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://tasty-server-2-2.onrender.com",
 });
 const useAxiosPublic = () => {
   return axiosPublic;
